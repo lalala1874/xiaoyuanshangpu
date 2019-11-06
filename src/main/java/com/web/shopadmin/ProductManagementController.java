0a2f1ef220917e00ecc6a1fb7fb0dad965dec058
lalala1874 <1229860674@qq.com>
2019-11-06 15:22:31 +0800

    2019-11-6

---
+++ src/main/java/com/web/shopadmin/ProductManagementController.java
@@ -0,0 +1,115 @@
+package com.web.shopadmin;
+
+import com.dto.ImageHolder;
+import com.dto.ProductExecution;
+import com.entity.Product;
+import com.entity.Shop;
+import com.enums.ProductStateEnum;
+import com.exceptions.ProductOperationException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.service.ProductService;
+import com.util.CodeUtil;
+import com.util.HttpServletRequestUtil;
+import com.util.ImageUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.multipart.MultipartRequest;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/shopadmin")
+public class ProductManagementController {
+
+    public final static int IMAGEMAXCOUNT = 6;
+
+    @Autowired
+    ProductService productService;
+
+    @RequestMapping(value = "/addProduct", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String, Object> addProduct(HttpServletRequest request) {
+        Map<String, Object> modelMap = new HashMap<>();
+        if (!CodeUtil.checkVerifyCode(request)) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "验证码错误");
+            return modelMap;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        Product product = null;
+        String productStr = HttpServletRequestUtil.getString(request, "productStr");
+        MultipartHttpServletRequest multipartHttpServletRequest = null;
+        ImageHolder thumbnail = null;
+        List<ImageHolder> productImgList = new ArrayList<>();
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+        try {
+            if (commonsMultipartResolver.isMultipart(request)) {
+                multipartHttpServletRequest = (MultipartHttpServletRequest) request;
+                CommonsMultipartFile thumbnailFile = (CommonsMultipartFile) multipartHttpServletRequest.getFile("thumbnail");
+                thumbnail = new ImageHolder(thumbnailFile.getOriginalFilename(), thumbnailFile.getInputStream());
+                for (int i = 0; i < IMAGEMAXCOUNT; i++) {
+                    CommonsMultipartFile productImgFile = (CommonsMultipartFile) multipartHttpServletRequest.getFile("productImg" + i);
+                    if (productImgFile != null) {
+                        ImageHolder productImg = new ImageHolder(productImgFile.getOriginalFilename(), productImgFile.getInputStream());
+                        productImgList.add(productImg);
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", "上传图片为空");
+                return modelMap;
+            }
+
+        } catch (Exception e) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", e.toString());
+            return modelMap;
+        }
+        try {
+            product = mapper.readValue(productStr, Product.class);
+
+        } catch (Exception e) {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", e.toString());
+            return modelMap;
+        }
+        if (product != null && thumbnail != null && productImgList.size() > 0) {
+            try {
+                Shop currentShop = (Shop) request.getSession().getAttribute("currentShop");
+                Shop shop = new Shop();
+                shop.setShopId(currentShop.getShopId());
+                product.setShop(shop);
+                ProductExecution pe = productService.addProduct(product, thumbnail, productImgList);
+                if (pe.getState() == ProductStateEnum.SUCCESS.getState()) {
+                    modelMap.put("success", true);
+                } else {
+                    modelMap.put("success", false);
+                    modelMap.put("errMsg", pe.getStateInfo());
+
+                }
+            } catch (ProductOperationException e) {
+                modelMap.put("success", false);
+                modelMap.put("errMsg", e.toString());
+                return modelMap;
+            }
+
+        } else {
+            modelMap.put("success", false);
+            modelMap.put("errMsg", "信息为空");
+        }
+        return modelMap;
+    }
+}
